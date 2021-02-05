00881d57aa1c3336f7d4309b0b9ea7b52fff1e55
Boernsman <bernhard.trinnes@nymea.io>
2021-02-05 10:59:57 +0100

    finished neuron refactoring

--- unipi/neuron.h
+++ unipi/neuron.h
@@ -38,7 +38,6 @@
 #include <QTimer>
 #include <QUuid>
 
-
 class Neuron : public NeuronCommon
 {
     Q_OBJECT
@@ -62,46 +61,12 @@ public:
     };
     Q_ENUM(NeuronTypes)
 
-    explicit Neuron(NeuronTypes neuronType, QModbusTcpClient *modbusInterface, QObject *parent = nullptr);
+    explicit Neuron(NeuronTypes neuronType, QModbusClient *modbusInterface, QObject *parent = nullptr);
     ~Neuron();
-
-    bool init();
     QString type();
 
-    QUuid setDigitalOutput(const QString &circuit, bool value);
-    QUuid setAnalogOutput(const QString &circuit, double value);
-    QUuid setUserLED(const QString &circuit, bool value);
-
-    bool getDigitalOutput(const QString &circuit);
-    bool getDigitalInput(const QString &circuit);
-
-    bool getAnalogOutput(const QString &circuit);
-    bool getAnalogInput(const QString &circuit);
-
-    bool getAllDigitalOutputs();
-    bool getAllDigitalInputs();
-    bool getAllAnalogInputs();
-    bool getAllAnalogOutputs();
-
-    bool getUserLED(const QString &circuit);
-
 private:
-    QModbusTcpClient *m_modbusInterface = nullptr;
     NeuronTypes m_neuronType = NeuronTypes::S103;
-
-    bool loadModbusMap();
-    bool modbusReadRequest(const QModbusDataUnit &request);
-    bool modbusWriteRequest(const Request &request);
-
-    bool getInputRegisters(QList<int> registers);
-    bool getHoldingRegisters(QList<int> registers);
-    bool getCoils(QList<int> registers);
-
-    bool getAnalogIO(const RegisterDescriptor &descriptor);
-
-public slots:
-    void onOutputPollingTimer();
-    void onInputPollingTimer();
+    bool loadModbusMap() override;
 };
-
 #endif // NEURON_H
