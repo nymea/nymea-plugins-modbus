ce9a98b91ef88ae32b067b56c91cb34993f0c931
Boernsman <you@example.com>
2021-04-21 18:49:07 +0200

    added translation

---
+++ energymeters/translations/56e95111-fb6b-4f63-9a0a-a5ee001e89ed-en_US.ts
@@ -0,0 +1,285 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>EnergyMeters</name>
+    <message>
+        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/energymeters/plugininfo.h" line="85"/>
+        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/energymeters/plugininfo.h" line="88"/>
+        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/energymeters/plugininfo.h" line="91"/>
+        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/energymeters/plugininfo.h" line="94"/>
+        <source>Active power</source>
+        <extracomment>The name of the ParamType (ThingClass: sdm630, EventType: currentPower, ID: {c824e97b-a6d1-4030-9d7a-00af6fb8e1c3})
+----------
+The name of the StateType ({c824e97b-a6d1-4030-9d7a-00af6fb8e1c3}) of ThingClass sdm630
+----------
+The name of the ParamType (ThingClass: pro380, EventType: currentPower, ID: {464eff60-11c2-46b7-98f5-1aa8172e5a2d})
+----------
+The name of the StateType ({464eff60-11c2-46b7-98f5-1aa8172e5a2d}) of ThingClass pro380</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/energymeters/plugininfo.h" line="97"/>
+        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/energymeters/plugininfo.h" line="100"/>
+        <source>Active power changed</source>
+        <extracomment>The name of the EventType ({c824e97b-a6d1-4030-9d7a-00af6fb8e1c3}) of ThingClass sdm630
+----------
+The name of the EventType ({464eff60-11c2-46b7-98f5-1aa8172e5a2d}) of ThingClass pro380</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/energymeters/plugininfo.h" line="103"/>
+        <source>B+G e-tech</source>
+        <extracomment>The name of the vendor ({215035fe-95e8-43d8-a52e-0a31b787d902})</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/energymeters/plugininfo.h" line="106"/>
+        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/energymeters/plugininfo.h" line="109"/>
+        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/energymeters/plugininfo.h" line="112"/>
+        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/energymeters/plugininfo.h" line="115"/>
+        <source>Connected</source>
+        <extracomment>The name of the ParamType (ThingClass: sdm630, EventType: connected, ID: {8050bd0b-1dad-4a7e-b632-c71ead3c9f8b})
+----------
+The name of the StateType ({8050bd0b-1dad-4a7e-b632-c71ead3c9f8b}) of ThingClass sdm630
+----------
+The name of the ParamType (ThingClass: pro380, EventType: connected, ID: {7f9bc504-0882-4b86-83b1-42fa345acfd9})
+----------
+The name of the StateType ({7f9bc504-0882-4b86-83b1-42fa345acfd9}) of ThingClass pro380</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/energymeters/plugininfo.h" line="118"/>
+        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/energymeters/plugininfo.h" line="121"/>
+        <source>Connected changed</source>
+        <extracomment>The name of the EventType ({8050bd0b-1dad-4a7e-b632-c71ead3c9f8b}) of ThingClass sdm630
+----------
+The name of the EventType ({7f9bc504-0882-4b86-83b1-42fa345acfd9}) of ThingClass pro380</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/energymeters/plugininfo.h" line="124"/>
+        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/energymeters/plugininfo.h" line="127"/>
+        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/energymeters/plugininfo.h" line="130"/>
+        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/energymeters/plugininfo.h" line="133"/>
+        <source>Current</source>
+        <extracomment>The name of the ParamType (ThingClass: sdm630, EventType: current, ID: {96bc65ce-5bde-4a69-9ebf-711d65c6501c})
+----------
+The name of the StateType ({96bc65ce-5bde-4a69-9ebf-711d65c6501c}) of ThingClass sdm630
+----------
+The name of the ParamType (ThingClass: pro380, EventType: current, ID: {1e077a3b-2dab-4ec4-ae96-ab49a564fe31})
+----------
+The name of the StateType ({1e077a3b-2dab-4ec4-ae96-ab49a564fe31}) of ThingClass pro380</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/energymeters/plugininfo.h" line="136"/>
+        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/energymeters/plugininfo.h" line="139"/>
+        <source>Current changed</source>
+        <extracomment>The name of the EventType ({96bc65ce-5bde-4a69-9ebf-711d65c6501c}) of ThingClass sdm630
+----------
+The name of the EventType ({1e077a3b-2dab-4ec4-ae96-ab49a564fe31}) of ThingClass pro380</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/energymeters/plugininfo.h" line="142"/>
+        <source>EnergyMeters</source>
+        <extracomment>The name of the plugin EnergyMeters ({56e95111-fb6b-4f63-9a0a-a5ee001e89ed})</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/energymeters/plugininfo.h" line="145"/>
+        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/energymeters/plugininfo.h" line="148"/>
+        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/energymeters/plugininfo.h" line="151"/>
+        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/energymeters/plugininfo.h" line="154"/>
+        <source>Frequency</source>
+        <extracomment>The name of the ParamType (ThingClass: sdm630, EventType: frequency, ID: {ab24f26c-dc15-4ec3-8d76-06a48285440b})
+----------
+The name of the StateType ({ab24f26c-dc15-4ec3-8d76-06a48285440b}) of ThingClass sdm630
+----------
+The name of the ParamType (ThingClass: pro380, EventType: frequency, ID: {bb6fd00c-3bbb-4977-bb8a-96787bb6f5c5})
+----------
+The name of the StateType ({bb6fd00c-3bbb-4977-bb8a-96787bb6f5c5}) of ThingClass pro380</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/energymeters/plugininfo.h" line="157"/>
+        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/energymeters/plugininfo.h" line="160"/>
+        <source>Frequency changed</source>
+        <extracomment>The name of the EventType ({ab24f26c-dc15-4ec3-8d76-06a48285440b}) of ThingClass sdm630
+----------
+The name of the EventType ({bb6fd00c-3bbb-4977-bb8a-96787bb6f5c5}) of ThingClass pro380</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/energymeters/plugininfo.h" line="163"/>
+        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/energymeters/plugininfo.h" line="166"/>
+        <source>Modbus RTU master</source>
+        <extracomment>The name of the ParamType (ThingClass: sdm630, Type: thing, ID: {d90e9292-d03c-4f2a-957e-5d965018c9c9})
+----------
+The name of the ParamType (ThingClass: pro380, Type: thing, ID: {6cdbec8c-21b9-42dc-b1ab-8901ac609482})</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/energymeters/plugininfo.h" line="169"/>
+        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/energymeters/plugininfo.h" line="172"/>
+        <source>Modbus slave address</source>
+        <extracomment>The name of the ParamType (ThingClass: sdm630, Type: thing, ID: {ac77ea98-b006-486e-a3e8-b30a483f26c1})
+----------
+The name of the ParamType (ThingClass: pro380, Type: thing, ID: {c75b2c31-6ec3-49ab-8c8f-5231d0a7e941})</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/energymeters/plugininfo.h" line="175"/>
+        <source>PRO380-Mod</source>
+        <extracomment>The name of the ThingClass ({d7c6440b-54f9-4cc0-a96b-9bb7304b3e77})</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/energymeters/plugininfo.h" line="178"/>
+        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/energymeters/plugininfo.h" line="181"/>
+        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/energymeters/plugininfo.h" line="184"/>
+        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/energymeters/plugininfo.h" line="187"/>
+        <source>Power factor</source>
+        <extracomment>The name of the ParamType (ThingClass: sdm630, EventType: powerFactor, ID: {31b9032f-f994-472b-94bd-44f9fb094801})
+----------
+The name of the StateType ({31b9032f-f994-472b-94bd-44f9fb094801}) of ThingClass sdm630
+----------
+The name of the ParamType (ThingClass: pro380, EventType: powerFactor, ID: {cdb34487-3d9b-492a-8c33-802f32a2e90e})
+----------
+The name of the StateType ({cdb34487-3d9b-492a-8c33-802f32a2e90e}) of ThingClass pro380</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/energymeters/plugininfo.h" line="190"/>
+        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/energymeters/plugininfo.h" line="193"/>
+        <source>Power factor changed</source>
+        <extracomment>The name of the EventType ({31b9032f-f994-472b-94bd-44f9fb094801}) of ThingClass sdm630
+----------
+The name of the EventType ({cdb34487-3d9b-492a-8c33-802f32a2e90e}) of ThingClass pro380</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/energymeters/plugininfo.h" line="196"/>
+        <source>SDM630Modbus</source>
+        <extracomment>The name of the ThingClass ({f37597bb-35fe-48f2-9617-343dd54c0903})</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/energymeters/plugininfo.h" line="199"/>
+        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/energymeters/plugininfo.h" line="202"/>
+        <source>Slave address</source>
+        <extracomment>The name of the ParamType (ThingClass: sdm630, Type: discovery, ID: {6ab43559-53ec-47ba-b8a0-8d3b7f8d90c2})
+----------
+The name of the ParamType (ThingClass: pro380, Type: discovery, ID: {a29f37f6-b344-4628-8ab4-8f4c18fada4a})</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/energymeters/plugininfo.h" line="205"/>
+        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/energymeters/plugininfo.h" line="208"/>
+        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/energymeters/plugininfo.h" line="211"/>
+        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/energymeters/plugininfo.h" line="214"/>
+        <source>Total energy consumed</source>
+        <extracomment>The name of the ParamType (ThingClass: sdm630, EventType: totalEnergyConsumed, ID: {98d858a8-22e8-4262-b5c7-25bb027942ad})
+----------
+The name of the StateType ({98d858a8-22e8-4262-b5c7-25bb027942ad}) of ThingClass sdm630
+----------
+The name of the ParamType (ThingClass: pro380, EventType: totalEnergyConsumed, ID: {f18fd596-b47f-44be-a0f0-6ca44369ebf5})
+----------
+The name of the StateType ({f18fd596-b47f-44be-a0f0-6ca44369ebf5}) of ThingClass pro380</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/energymeters/plugininfo.h" line="217"/>
+        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/energymeters/plugininfo.h" line="220"/>
+        <source>Total energy consumed changed</source>
+        <extracomment>The name of the EventType ({98d858a8-22e8-4262-b5c7-25bb027942ad}) of ThingClass sdm630
+----------
+The name of the EventType ({f18fd596-b47f-44be-a0f0-6ca44369ebf5}) of ThingClass pro380</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/energymeters/plugininfo.h" line="223"/>
+        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/energymeters/plugininfo.h" line="226"/>
+        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/energymeters/plugininfo.h" line="229"/>
+        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/energymeters/plugininfo.h" line="232"/>
+        <source>Total energy produced</source>
+        <extracomment>The name of the ParamType (ThingClass: sdm630, EventType: totalEnergyProduced, ID: {e469b3ff-a4c2-42da-af35-ccafaef214af})
+----------
+The name of the StateType ({e469b3ff-a4c2-42da-af35-ccafaef214af}) of ThingClass sdm630
+----------
+The name of the ParamType (ThingClass: pro380, EventType: totalEnergyProduced, ID: {112911c9-14e0-4c83-ac92-f2ceb3bdecdf})
+----------
+The name of the StateType ({112911c9-14e0-4c83-ac92-f2ceb3bdecdf}) of ThingClass pro380</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/energymeters/plugininfo.h" line="235"/>
+        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/energymeters/plugininfo.h" line="238"/>
+        <source>Total energy produced changed</source>
+        <extracomment>The name of the EventType ({e469b3ff-a4c2-42da-af35-ccafaef214af}) of ThingClass sdm630
+----------
+The name of the EventType ({112911c9-14e0-4c83-ac92-f2ceb3bdecdf}) of ThingClass pro380</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/energymeters/plugininfo.h" line="241"/>
+        <source>Update interval</source>
+        <extracomment>The name of the ParamType (ThingClass: energyMeters, Type: plugin, ID: {eaa84c3c-06b8-4642-a40b-c2efbe6aae66})</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/energymeters/plugininfo.h" line="244"/>
+        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/energymeters/plugininfo.h" line="247"/>
+        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/energymeters/plugininfo.h" line="250"/>
+        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/energymeters/plugininfo.h" line="253"/>
+        <source>Voltage</source>
+        <extracomment>The name of the ParamType (ThingClass: sdm630, EventType: voltage, ID: {4636ec5c-fcb9-45b7-ad68-2818cb615ce1})
+----------
+The name of the StateType ({4636ec5c-fcb9-45b7-ad68-2818cb615ce1}) of ThingClass sdm630
+----------
+The name of the ParamType (ThingClass: pro380, EventType: voltage, ID: {04dba21a-7447-46b9-b9ae-095e5769e511})
+----------
+The name of the StateType ({04dba21a-7447-46b9-b9ae-095e5769e511}) of ThingClass pro380</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/energymeters/plugininfo.h" line="256"/>
+        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/energymeters/plugininfo.h" line="259"/>
+        <source>Voltage changed</source>
+        <extracomment>The name of the EventType ({4636ec5c-fcb9-45b7-ad68-2818cb615ce1}) of ThingClass sdm630
+----------
+The name of the EventType ({04dba21a-7447-46b9-b9ae-095e5769e511}) of ThingClass pro380</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/energymeters/plugininfo.h" line="262"/>
+        <source>inepro Metering</source>
+        <extracomment>The name of the vendor ({64f4df0f-18ce-409c-bf32-84a086c691ca})</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>IntegrationPluginEnergyMeters</name>
+    <message>
+        <location filename="../integrationpluginenergymeters.cpp" line="102"/>
+        <source>No Modbus RTU interface available.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../integrationpluginenergymeters.cpp" line="109"/>
+        <source>Modbus slave address must be between 1 and 254</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../integrationpluginenergymeters.cpp" line="145"/>
+        <source>Slave address not valid, must be between 1 and 254</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../integrationpluginenergymeters.cpp" line="151"/>
+        <source>Modbus RTU resource not available.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
