4a7cd2600cd32c00095daf366e3bc5c8be2b7fe7
Simon Stürz <simon.stuerz@nymea.io>
2021-06-10 13:59:22 +0200

    Add thermostat interface, extend values and implement execute action

--- mtec/mtec.cpp
+++ mtec/mtec.cpp
@@ -51,6 +51,37 @@ MTec::~MTec()
     m_modbusMaster->disconnectDevice();
 }
 
+QHostAddress MTec::hostAddress() const
+{
+    return m_hostAddress;
+}
+
+bool MTec::connected() const
+{
+    return m_modbusMaster->connected();
+}
+
+QModbusReply *MTec::setTargetRoomTemperature(double targetRoomTemperature)
+{
+    QVector<quint16> values;
+    values << static_cast<quint16>(qRound(targetRoomTemperature * 10));
+
+    QModbusDataUnit request = QModbusDataUnit(QModbusDataUnit::RegisterType::HoldingRegisters, RegisterTargetRoomTemperature, values.length());
+    request.setValues(values);
+    QModbusReply *reply = m_modbusMaster->sendWriteRequest(request, MTec::ModbusUnitID);
+    return reply;
+}
+
+QModbusReply *MTec::setSmartHomeEnergy(quint16 smartHomeEnergy)
+{
+    QVector<quint16> values;
+    values << smartHomeEnergy;
+    QModbusDataUnit request = QModbusDataUnit(QModbusDataUnit::RegisterType::HoldingRegisters, RegisterActualExcessEnergySmartHome, values.length());
+    request.setValues(values);
+    QModbusReply *reply = m_modbusMaster->sendWriteRequest(request, MTec::ModbusUnitID);
+    return reply;
+}
+
 bool MTec::connectDevice()
 {
     return m_modbusMaster->connectDevice();
@@ -67,7 +98,7 @@ void MTec::updateValues()
         return;
     }
 
-    m_modbusMaster->readHoldingRegister(MTec::ModbusUnitID, RegisterHotWaterTankTemperature, 1);
+    m_modbusMaster->readHoldingRegister(MTec::ModbusUnitID, RegisterRoomTemperature, 1);
 }
 
 void MTec::onModbusError()
@@ -80,10 +111,24 @@ void MTec::onReceivedHoldingRegister(int slaveAddress, int modbusRegister, const
     Q_UNUSED(slaveAddress);
 
     switch (modbusRegister) {
+    case RegisterRoomTemperature:
+        if (value.length() == 1) {
+            m_roomTemperature = value[0] / 10.0;
+            emit roomTemperatureChanged(m_roomTemperature);
+        }
+        m_modbusMaster->readHoldingRegister(MTec::ModbusUnitID, RegisterTargetRoomTemperature, 1);
+        break;
+    case RegisterTargetRoomTemperature:
+        if (value.length() == 1) {
+            m_targetRoomTemperature = value[0] / 10.0;
+            emit targetRoomTemperatureChanged(m_targetRoomTemperature);
+        }
+        m_modbusMaster->readHoldingRegister(MTec::ModbusUnitID, RegisterHotWaterTankTemperature, 1);
+        break;
     case RegisterHotWaterTankTemperature:
         if (value.length() == 1) {
-            m_waterTankTemperature = value[0] / 10.0;
-            emit waterTankTemperatureChanged(m_waterTankTemperature);
+            m_waterTankTopTemperature = value[0] / 10.0;
+            emit waterTankTopTemperatureChanged(m_waterTankTopTemperature);
         }
         m_modbusMaster->readHoldingRegister(MTec::ModbusUnitID, RegisterBufferTankTemperature, 1);
         break;
@@ -92,11 +137,18 @@ void MTec::onReceivedHoldingRegister(int slaveAddress, int modbusRegister, const
             m_bufferTankTemperature = value[0] / 10.0;
             emit bufferTankTemperatureChanged(m_bufferTankTemperature);
         }
+        m_modbusMaster->readHoldingRegister(MTec::ModbusUnitID, RegisterTotalAccumulatedHeatingEnergy, 1);
+        break;
+    case RegisterTotalAccumulatedHeatingEnergy:
+        if (value.length() == 1) {
+            m_totalAccumulatedElectricalEnergy = value[0];
+            emit totalAccumulatedElectricalEnergyChanged(m_totalAccumulatedElectricalEnergy);
+        }
         m_modbusMaster->readHoldingRegister(MTec::ModbusUnitID, RegisterTotalAccumulatedElectricalEnergy, 1);
         break;
     case RegisterTotalAccumulatedElectricalEnergy:
         if (value.length() == 1) {
-            m_totalAccumulatedElectricalEnergy = value[0] / 100.0;
+            m_totalAccumulatedElectricalEnergy = value[0];
             emit totalAccumulatedElectricalEnergyChanged(m_totalAccumulatedElectricalEnergy);
         }
         m_modbusMaster->readHoldingRegister(MTec::ModbusUnitID, RegisterHeatpumpState, 1);
@@ -110,21 +162,28 @@ void MTec::onReceivedHoldingRegister(int slaveAddress, int modbusRegister, const
         break;
     case RegisterHeatMeterPowerConsumption:
         if (value.length() == 1) {
-            m_heatMeterPowerConsumption = value[0] / 100.0;
+            m_heatMeterPowerConsumption = value[0];
             emit heatMeterPowerConsumptionChanged(m_heatMeterPowerConsumption);
         }
         m_modbusMaster->readHoldingRegister(MTec::ModbusUnitID, RegisterEnergyMeterPowerConsumption, 1);
         break;
     case RegisterEnergyMeterPowerConsumption:
         if (value.length() == 1) {
-            m_energyMeterPowerConsumption = value[0] / 100.0;
+            m_energyMeterPowerConsumption = value[0];
             emit energyMeterPowerConsumptionChanged(m_energyMeterPowerConsumption);
         }
         m_modbusMaster->readHoldingRegister(MTec::ModbusUnitID, RegisterActualExcessEnergySmartHome, 1);
         break;
     case RegisterActualExcessEnergySmartHome:
         if (value.length() == 1) {
-            m_actualExcessEnergySmartHome = value[0] / 100.0;
+            m_actualExcessEnergySmartHome = value[0];
+            emit actualExcessEnergySmartHomeChanged(m_actualExcessEnergySmartHome);
+        }
+        m_modbusMaster->readHoldingRegister(MTec::ModbusUnitID, RegisterActualExcessEnergySmartHomeElectricityMeter, 1);
+        break;
+    case RegisterActualExcessEnergySmartHomeElectricityMeter:
+        if (value.length() == 1) {
+            m_actualExcessEnergySmartHomeElectricityMeter = value[0];
             emit actualExcessEnergySmartHomeChanged(m_actualExcessEnergySmartHome);
         }
         m_modbusMaster->readHoldingRegister(MTec::ModbusUnitID, RegisterActualOutdoorTemperature, 1);
