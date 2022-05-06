ccdfe3e0cdb000b50aee133b767ba7bab7c42582
Simon Stürz <simon.stuerz@nymea.io>
2022-05-06 11:55:30 +0200

    Update mypv plugin to libnyma-modbus

--- mypv/integrationpluginmypv.h
+++ mypv/integrationpluginmypv.h
@@ -31,10 +31,10 @@
 #ifndef INTEGRATIONPLUGINMYPV_H
 #define INTEGRATIONPLUGINMYPV_H
 
-#include "integrations/integrationplugin.h"
-#include "plugintimer.h"
+#include <integrations/integrationplugin.h>
+#include <plugintimer.h>
 
-#include "../modbus/modbustcpmaster.h"
+#include <modbustcpmaster.h>
 
 #include <QUdpSocket>
 #include <QUuid>
