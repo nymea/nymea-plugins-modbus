4cd6118d1289fd7f72a7f445f6cca9c15391199f
Michael Zanetti <michael_zanetti@gmx.net>
2022-08-05 18:36:56 +0200

    Fix generating WO registers

--- libnymea-modbus/tools/connectiontool/toolcommon.py
+++ libnymea-modbus/tools/connectiontool/toolcommon.py
@@ -352,7 +352,7 @@ def writeBlockGetMethodDeclarations(fileDescriptor, registerDefinitions):
 
 def writePropertyUpdateMethodDeclarations(fileDescriptor, registerDefinitions):
     for registerDefinition in registerDefinitions:
-        if 'readSchedule' in registerDefinition and registerDefinition['readSchedule'] == 'init':
+        if 'readSchedule' not in registerDefinition or registerDefinition['readSchedule'] == 'init':
             continue
 
         propertyName = registerDefinition['id']
@@ -427,6 +427,9 @@ def writeBlocksUpdateMethodDeclarations(fileDescriptor, blockDefinitions):
 
 def writeRegistersDebugLine(fileDescriptor, debugObjectParamName, registerDefinitions):
     for registerDefinition in registerDefinitions:
+        if registerDefinition['access'] == 'WO':
+            continue
+
         propertyName = registerDefinition['id']
         propertyTyp = getCppDataType(registerDefinition)
         line = ('"    - %s: " << %s->%s()' % (registerDefinition['description'], debugObjectParamName, propertyName))
