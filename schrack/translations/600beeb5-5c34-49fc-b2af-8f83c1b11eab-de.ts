0ef0a8daf7a18cd67465e19fac68dde54b8fc01f
Michael Zanetti <michael_zanetti@gmx.net>
2023-03-07 15:43:15 +0100

    Update translations

--- schrack/translations/600beeb5-5c34-49fc-b2af-8f83c1b11eab-de.ts
+++ schrack/translations/600beeb5-5c34-49fc-b2af-8f83c1b11eab-de.ts
@@ -1,22 +1,22 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1">
+<TS version="2.1" language="de">
 <context>
     <name>IntegrationPluginSchrack</name>
     <message>
         <location filename="../integrationpluginschrack.cpp" line="51"/>
         <source>No modbus RTU master with appropriate settings found. Please set up a modbus RTU master with a baudrate of 57600, 8 data bis, 1 stop bit and no parity first.</source>
-        <translation type="unfinished"></translation>
+        <translation>Es wurde mein Modbus RTU Master mit passenden Einstellungen gefunden. Bitte richte einen Modbus RTU Master mit einer Baudrate von 57600, 8 Daten-Bits, 1 Stop-Bit und keiner Parität ein.</translation>
     </message>
     <message>
         <location filename="../integrationpluginschrack.cpp" line="87"/>
         <source>The Modbus address not valid. It must be a value between 1 and 254.</source>
-        <translation type="unfinished"></translation>
+        <translation>Die Modbus Addresse ist ungültig. Sie muss zwischen 1 und 254 sein.</translation>
     </message>
     <message>
         <location filename="../integrationpluginschrack.cpp" line="94"/>
         <source>The Modbus RTU resource is not available.</source>
-        <translation type="unfinished"></translation>
+        <translation>Die Modbus RTU Ressource ist nicht verfügbar.</translation>
     </message>
 </context>
 <context>
@@ -25,7 +25,7 @@
         <location filename="../../../build/nymea-plugins-modbus-Desktop-Debug/schrack/plugininfo.h" line="40"/>
         <source>Charging</source>
         <extracomment>The name of the StateType ({b59b4b4d-2cdb-4534-bf86-90123ae9bb1a}) of ThingClass cion</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Ladend</translation>
     </message>
     <message>
         <location filename="../../../build/nymea-plugins-modbus-Desktop-Debug/schrack/plugininfo.h" line="43"/>
@@ -34,25 +34,25 @@
         <extracomment>The name of the ParamType (ThingClass: cion, ActionType: power, ID: {61aea53a-bf8d-4fe6-859a-f1687e15d190})
 ----------
 The name of the StateType ({61aea53a-bf8d-4fe6-859a-f1687e15d190}) of ThingClass cion</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Laden freigegeben</translation>
     </message>
     <message>
         <location filename="../../../build/nymea-plugins-modbus-Desktop-Debug/schrack/plugininfo.h" line="49"/>
         <source>Connected</source>
         <extracomment>The name of the StateType ({b7aa8e49-a6c0-4b48-b65e-47259686185f}) of ThingClass cion</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Verbunden</translation>
     </message>
     <message>
         <location filename="../../../build/nymea-plugins-modbus-Desktop-Debug/schrack/plugininfo.h" line="52"/>
         <source>Enable or disable charging</source>
         <extracomment>The name of the ActionType ({61aea53a-bf8d-4fe6-859a-f1687e15d190}) of ThingClass cion</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Ladefreigabe erteilen/entfernen</translation>
     </message>
     <message>
         <location filename="../../../build/nymea-plugins-modbus-Desktop-Debug/schrack/plugininfo.h" line="55"/>
         <source>Firmware version</source>
         <extracomment>The name of the StateType ({33780b19-4855-4b5c-bd84-cec8400309be}) of ThingClass cion</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Firmware-Version</translation>
     </message>
     <message>
         <location filename="../../../build/nymea-plugins-modbus-Desktop-Debug/schrack/plugininfo.h" line="58"/>
@@ -61,49 +61,49 @@ The name of the StateType ({61aea53a-bf8d-4fe6-859a-f1687e15d190}) of ThingClass
         <extracomment>The name of the ParamType (ThingClass: cion, ActionType: maxChargingCurrent, ID: {221af869-a796-46c2-a5e0-27c8972a0bf2})
 ----------
 The name of the StateType ({221af869-a796-46c2-a5e0-27c8972a0bf2}) of ThingClass cion</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Maximaler Ladestrom</translation>
     </message>
     <message>
         <location filename="../../../build/nymea-plugins-modbus-Desktop-Debug/schrack/plugininfo.h" line="64"/>
         <source>Modbus RTU master</source>
         <extracomment>The name of the ParamType (ThingClass: cion, Type: thing, ID: {636241a9-4838-48ae-bcc8-3427ac3fc102})</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Modbus RTU Master</translation>
     </message>
     <message>
         <location filename="../../../build/nymea-plugins-modbus-Desktop-Debug/schrack/plugininfo.h" line="67"/>
         <source>Modbus slave address</source>
         <extracomment>The name of the ParamType (ThingClass: cion, Type: thing, ID: {dac10e08-734c-4e71-a5d6-0d2a1f416ca6})</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Modbus Slave Adresse</translation>
     </message>
     <message>
         <location filename="../../../build/nymea-plugins-modbus-Desktop-Debug/schrack/plugininfo.h" line="70"/>
         <source>Plugged in</source>
         <extracomment>The name of the StateType ({13423618-4314-49be-b48c-42d9415199a8}) of ThingClass cion</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Eingesteckt</translation>
     </message>
     <message>
         <location filename="../../../build/nymea-plugins-modbus-Desktop-Debug/schrack/plugininfo.h" line="73"/>
         <source>Schrack</source>
         <extracomment>The name of the plugin schrack ({600beeb5-5c34-49fc-b2af-8f83c1b11eab})</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Schrack</translation>
     </message>
     <message>
         <location filename="../../../build/nymea-plugins-modbus-Desktop-Debug/schrack/plugininfo.h" line="76"/>
         <source>Schrack  GmbH</source>
         <extracomment>The name of the vendor ({cadbbbc5-216f-4d25-a8ad-ccf653d1518f})</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Schrack GmbH</translation>
     </message>
     <message>
         <location filename="../../../build/nymea-plugins-modbus-Desktop-Debug/schrack/plugininfo.h" line="79"/>
         <source>Set maximum charging current</source>
         <extracomment>The name of the ActionType ({221af869-a796-46c2-a5e0-27c8972a0bf2}) of ThingClass cion</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Setze maximalen Ladestrom</translation>
     </message>
     <message>
         <location filename="../../../build/nymea-plugins-modbus-Desktop-Debug/schrack/plugininfo.h" line="82"/>
         <source>i-CHARGE CION</source>
         <extracomment>The name of the ThingClass ({075d389d-3330-4d0b-9649-9f085120ca40})</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>i-CHARGE CION</translation>
     </message>
 </context>
 </TS>
