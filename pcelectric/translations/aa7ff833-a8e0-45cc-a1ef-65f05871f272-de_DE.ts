d90edc7aaed695dd90dbbda2ea7798a042aa0aff
Simon Stürz <simon.stuerz@nymea.io>
2025-05-05 13:17:31 +0200

    PCE: Add new settings and adopt to firmware 0.22

--- pcelectric/translations/aa7ff833-a8e0-45cc-a1ef-65f05871f272-de_DE.ts
+++ pcelectric/translations/aa7ff833-a8e0-45cc-a1ef-65f05871f272-de_DE.ts
@@ -8,41 +8,42 @@
         <source>The network device discovery is not available.</source>
         <translation>Die Netzwerk Suche ist nicht verfügbar.</translation>
     </message>
-    <message>
-        <location filename="../integrationpluginpcelectric.cpp" line="103"/>
-        <source>The MAC address is not known. Please reconfigure the thing.</source>
-        <translation>Die MAC-Adresse ist nicht bekannt. Bitte richte das Gerät erneut ein.</translation>
-    </message>
 </context>
 <context>
     <name>PcElectric</name>
     <message>
-        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/pcelectric/plugininfo.h" line="48"/>
+        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/pcelectric/plugininfo.h" line="56"/>
         <source>1</source>
         <extracomment>The name of a possible value of StateType {d91f7d96-2599-400a-91da-d164477098b7} of ThingClass ev11</extracomment>
         <translation>1</translation>
     </message>
     <message>
-        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/pcelectric/plugininfo.h" line="51"/>
+        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/pcelectric/plugininfo.h" line="59"/>
         <source>3</source>
         <extracomment>The name of a possible value of StateType {d91f7d96-2599-400a-91da-d164477098b7} of ThingClass ev11</extracomment>
         <translation>3</translation>
     </message>
     <message>
-        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/pcelectric/plugininfo.h" line="54"/>
+        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/pcelectric/plugininfo.h" line="62"/>
         <source>Active phases</source>
         <extracomment>The name of the StateType ({bca88c23-e940-40c1-afca-eb511fd17aab}) of ThingClass ev11</extracomment>
         <translation>Aktive Phasen</translation>
     </message>
     <message>
-        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/pcelectric/plugininfo.h" line="57"/>
+        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/pcelectric/plugininfo.h" line="65"/>
+        <source>Active power</source>
+        <extracomment>The name of the StateType ({bbe97e15-8fa7-42e0-9023-1d41425ccbee}) of ThingClass ev11</extracomment>
+        <translation>Aktive Leistung</translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/pcelectric/plugininfo.h" line="68"/>
         <source>Charging</source>
         <extracomment>The name of the StateType ({b7972cd7-471a-46bd-ab99-f49997f12309}) of ThingClass ev11</extracomment>
         <translation>Lädt</translation>
     </message>
     <message>
-        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/pcelectric/plugininfo.h" line="60"/>
-        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/pcelectric/plugininfo.h" line="63"/>
+        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/pcelectric/plugininfo.h" line="71"/>
+        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/pcelectric/plugininfo.h" line="74"/>
         <source>Charging enabled</source>
         <extracomment>The name of the ParamType (ThingClass: ev11, ActionType: power, ID: {c12a7a27-fa56-450c-a1ec-717c868554f2})
 ----------
@@ -50,14 +51,14 @@ The name of the StateType ({c12a7a27-fa56-450c-a1ec-717c868554f2}) of ThingClass
         <translation>Laden eingeschalten</translation>
     </message>
     <message>
-        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/pcelectric/plugininfo.h" line="66"/>
+        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/pcelectric/plugininfo.h" line="77"/>
         <source>Connected</source>
         <extracomment>The name of the StateType ({ca8d680c-c2f8-456a-a246-9c6cd64e25a7}) of ThingClass ev11</extracomment>
         <translation>Verbunden</translation>
     </message>
     <message>
-        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/pcelectric/plugininfo.h" line="69"/>
-        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/pcelectric/plugininfo.h" line="72"/>
+        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/pcelectric/plugininfo.h" line="80"/>
+        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/pcelectric/plugininfo.h" line="83"/>
         <source>Desired phase count</source>
         <extracomment>The name of the ParamType (ThingClass: ev11, ActionType: desiredPhaseCount, ID: {d91f7d96-2599-400a-91da-d164477098b7})
 ----------
@@ -65,50 +66,80 @@ The name of the StateType ({d91f7d96-2599-400a-91da-d164477098b7}) of ThingClass
         <translation>Gewünschte Phasenanzahl</translation>
     </message>
     <message>
-        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/pcelectric/plugininfo.h" line="75"/>
+        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/pcelectric/plugininfo.h" line="86"/>
+        <source>Digital input flag</source>
+        <extracomment>The name of the StateType ({c606c59b-6b3e-4de8-aaf8-aea4742200d9}) of ThingClass ev11</extracomment>
+        <translation>Digitale Eingänge Werte</translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/pcelectric/plugininfo.h" line="89"/>
+        <source>Digital input mode</source>
+        <extracomment>The name of the ParamType (ThingClass: ev11, Type: settings, ID: {930e0bf9-0038-436d-9eae-5c0f1cb28825})</extracomment>
+        <translation>Digitale Eingänge Modi</translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/pcelectric/plugininfo.h" line="92"/>
         <source>Enable or disable charging</source>
         <extracomment>The name of the ActionType ({c12a7a27-fa56-450c-a1ec-717c868554f2}) of ThingClass ev11</extracomment>
         <translation>Laden starten/stoppen</translation>
     </message>
     <message>
-        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/pcelectric/plugininfo.h" line="78"/>
+        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/pcelectric/plugininfo.h" line="95"/>
         <source>Error</source>
         <extracomment>The name of the StateType ({2ea1a53f-b2b0-452d-8060-cdb114db05a7}) of ThingClass ev11</extracomment>
         <translation>Fehler</translation>
     </message>
     <message>
-        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/pcelectric/plugininfo.h" line="81"/>
+        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/pcelectric/plugininfo.h" line="98"/>
         <source>Firmware version</source>
         <extracomment>The name of the StateType ({142b4276-e2e9-4149-adc4-89d9d3e31117}) of ThingClass ev11</extracomment>
         <translation>Firmware Version</translation>
     </message>
     <message>
-        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/pcelectric/plugininfo.h" line="96"/>
+        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/pcelectric/plugininfo.h" line="101"/>
+        <source>Force charging resume</source>
+        <extracomment>The name of the ParamType (ThingClass: ev11, Type: settings, ID: {5cece964-fe64-4e26-86ee-ba8fbbedd523})</extracomment>
+        <translation>Erzwinge Ladefortsezung</translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/pcelectric/plugininfo.h" line="104"/>
+        <source>Host address</source>
+        <extracomment>The name of the ParamType (ThingClass: ev11, Type: thing, ID: {aa18f1ae-e2d5-4918-9230-ab3f6fb49010})</extracomment>
+        <translation>IP Adresse</translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/pcelectric/plugininfo.h" line="107"/>
+        <source>Host name</source>
+        <extracomment>The name of the ParamType (ThingClass: ev11, Type: thing, ID: {a8b81e4a-f4ec-479b-9cb8-19147759c4cc})</extracomment>
+        <translation>Hostname</translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/pcelectric/plugininfo.h" line="122"/>
         <source>Onboard temperature</source>
         <extracomment>The name of the StateType ({bb092562-377e-458e-bb8a-735af9036652}) of ThingClass ev11</extracomment>
         <translation>Temperatur</translation>
     </message>
     <message>
-        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/pcelectric/plugininfo.h" line="105"/>
+        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/pcelectric/plugininfo.h" line="131"/>
         <source>PCE EV11.3</source>
         <extracomment>The name of the ThingClass ({88d96940-a940-4a07-8176-5e6aba7ca832})</extracomment>
         <translation>PCE EV11.3</translation>
     </message>
     <message>
-        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/pcelectric/plugininfo.h" line="84"/>
+        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/pcelectric/plugininfo.h" line="110"/>
         <source>LED brightness</source>
         <extracomment>The name of the ParamType (ThingClass: ev11, Type: settings, ID: {3a1329a2-84cc-47b9-a6c2-e96fdfd0c454})</extracomment>
         <translation>LED Helligkeit</translation>
     </message>
     <message>
-        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/pcelectric/plugininfo.h" line="87"/>
+        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/pcelectric/plugininfo.h" line="113"/>
         <source>MAC address</source>
         <extracomment>The name of the ParamType (ThingClass: ev11, Type: thing, ID: {0a3f8d12-9d33-4ae2-b763-9568f32e8da1})</extracomment>
         <translation>MAC Adresse</translation>
     </message>
     <message>
-        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/pcelectric/plugininfo.h" line="90"/>
-        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/pcelectric/plugininfo.h" line="93"/>
+        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/pcelectric/plugininfo.h" line="116"/>
+        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/pcelectric/plugininfo.h" line="119"/>
         <source>Maximum charging current</source>
         <extracomment>The name of the ParamType (ThingClass: ev11, ActionType: maxChargingCurrent, ID: {b5bbf23c-06db-463b-bb5c-3aea38e18818})
 ----------
@@ -116,43 +147,55 @@ The name of the StateType ({b5bbf23c-06db-463b-bb5c-3aea38e18818}) of ThingClass
         <translation>Maximaler Ladestrom</translation>
     </message>
     <message>
-        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/pcelectric/plugininfo.h" line="99"/>
+        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/pcelectric/plugininfo.h" line="125"/>
         <source>PC Electric</source>
         <extracomment>The name of the plugin PcElectric ({aa7ff833-a8e0-45cc-a1ef-65f05871f272})</extracomment>
         <translation>PC Electric</translation>
     </message>
     <message>
-        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/pcelectric/plugininfo.h" line="102"/>
+        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/pcelectric/plugininfo.h" line="128"/>
         <source>PC Electric GmbH</source>
         <extracomment>The name of the vendor ({b365937b-f1d6-46bf-9ff1-e787373b8aa6})</extracomment>
         <translation>PC Electric GmbH</translation>
     </message>
     <message>
-        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/pcelectric/plugininfo.h" line="108"/>
+        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/pcelectric/plugininfo.h" line="134"/>
+        <source>Phase autoswitch minimal charging time</source>
+        <extracomment>The name of the ParamType (ThingClass: ev11, Type: settings, ID: {7b0b5375-7980-4f99-9542-1dcb0390fb22})</extracomment>
+        <translation>Mindestladezeit nach Phasenumschaltung</translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/pcelectric/plugininfo.h" line="137"/>
+        <source>Phase autoswitch pause</source>
+        <extracomment>The name of the ParamType (ThingClass: ev11, Type: settings, ID: {a5354c66-0a7a-44a8-bea0-0305b91b63cf})</extracomment>
+        <translation>Pause nach Phasenumschaltung</translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/pcelectric/plugininfo.h" line="140"/>
         <source>Plugged in</source>
         <extracomment>The name of the StateType ({50164bbd-9802-4cf6-82de-626b74293a1b}) of ThingClass ev11</extracomment>
         <translation>Angesteckt</translation>
     </message>
     <message>
-        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/pcelectric/plugininfo.h" line="111"/>
+        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/pcelectric/plugininfo.h" line="143"/>
         <source>Serial number</source>
         <extracomment>The name of the ParamType (ThingClass: ev11, Type: thing, ID: {db834ca7-934a-473b-8bd8-c641ff0ea879})</extracomment>
         <translation>Seriennummer</translation>
     </message>
     <message>
-        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/pcelectric/plugininfo.h" line="114"/>
+        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/pcelectric/plugininfo.h" line="146"/>
         <source>Session energy</source>
         <extracomment>The name of the StateType ({3da3ee80-e9e7-4237-85a6-b4adcb2f483b}) of ThingClass ev11</extracomment>
         <translation>Geladene Energie</translation>
     </message>
     <message>
-        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/pcelectric/plugininfo.h" line="117"/>
+        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/pcelectric/plugininfo.h" line="149"/>
         <source>Set desired phase count</source>
         <extracomment>The name of the ActionType ({d91f7d96-2599-400a-91da-d164477098b7}) of ThingClass ev11</extracomment>
         <translation>Setze gewünschte Phasenanzahl</translation>
     </message>
     <message>
-        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/pcelectric/plugininfo.h" line="120"/>
+        <location filename="../../../build-nymea-plugins-modbus-Desktop-Debug/pcelectric/plugininfo.h" line="152"/>
         <source>Set maximum charging current</source>
         <extracomment>The name of the ActionType ({b5bbf23c-06db-463b-bb5c-3aea38e18818}) of ThingClass ev11</extracomment>
         <translation>Setze maximalen Ladestrom</translation>
